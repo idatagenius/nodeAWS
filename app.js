1890de5214a285cacb4e38fbbf0ed6a2ea08818a
idatagenius <67245775+idatagenius@users.noreply.github.com>
2020-12-08 22:47:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/style', (req, res) => {
     res.setHeader("Content-Type", "text/css");
     res.send("@font-face {  font-family: 'SAP Fiori Icons';  src:    url('https://vipulstorageacc.z13.web.core.windows.net/SAP-icons.ttf') format('truetype');  font-weight: normal;  font-style: normal;}")
