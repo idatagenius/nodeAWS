29bb78667a4bc6314e6d61ec1d23f886d5ba7d34
idatagenius <67245775+idatagenius@users.noreply.github.com>
2020-12-08 22:40:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+
+app.get('/style', (req, res) => {
+    res.send("@font-face {  font-family: 'SAP Fiori Icons';  src:    url('https://vipulstorageacc.z13.web.core.windows.net/SAP-icons.ttf') format('truetype');  font-weight: normal;  font-style: normal;}")
+});
+
 app.get('/', (req, res) => {
     res.send("The home page")
 });
+
+
 
 const port = process.env.port || 8080;
 
