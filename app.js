41414d13cbdb207e9b86ce8bb5dce4a92603dc02
idatagenius <67245775+idatagenius@users.noreply.github.com>
2020-12-08 22:56:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,22 +7,9 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/index', (req, res) => {
+app.get('/indexpage', (req, res) => {
     res.setHeader("Content-Type", "text/html");
-    res.send("
-<!DOCTYPE html>
-<html lang='en'>
-<head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-    <title>Document</title>
-    <link rel='stylesheet' href='https://dtov41hw3tm6v.cloudfront.net/style' />
-</head>
-<body>
-    <span style='font-family: SAP Fiori Icons;'></span>
-</body>
-</html>")
+    res.send("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='ie=edge'>  <title>Document</title><link rel='stylesheet' href='https://dtov41hw3tm6v.cloudfront.net/style' /></head><body><span style='font-family: SAP Fiori Icons;'></span></body></html>")
 });
 
 app.get('/style', (req, res) => {
