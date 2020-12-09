a0effa113e055eeda95fbfd3bfac264000679ff4
Aditi Desai <idatagenius@gmail.com>
2020-12-08 21:51:15 -0500

    Minor change

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 const port = process.env.port || 8080;
 
 app.listen(port, () => {
-    console.log('Michael Scott in 2005: Whassup? \n Whatsapp executives: *thinking of what to name their company*')
+    console.log('Wasssupp')
 });
