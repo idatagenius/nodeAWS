d15d992f90ecf216da2fc6293d688c9a1f4c3493
Aditi Desai <idatagenius@gmail.com>
2020-12-08 21:19:29 -0500

    chaning port to a variable

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-
 app.get('/', (req, res) => {
     res.send("The home page")
 });
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
     console.log('Michael Scott in 2005: Whassup? \n Whatsapp executives: *thinking of what to name their company*')
 });
